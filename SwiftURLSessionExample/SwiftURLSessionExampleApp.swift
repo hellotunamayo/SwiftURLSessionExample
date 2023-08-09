4e7bc0328f04ed30fde5f23eddf1a0bdd1a5dd79
hellotunamayo <yoo@minyoungyoo.com>
2023-08-09 19:53:10 +0900

    Initial Commit

---
+++ SwiftURLSessionExample/SwiftURLSessionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftURLSessionExampleApp.swift
+//  SwiftURLSessionExample
+//
+//  Created by Minyoung Yoo on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftURLSessionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
